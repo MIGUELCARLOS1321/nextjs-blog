09c30852279b73a512b6dba352633201c781ca28
MIGUELCARLOS1321 <144299046+MIGUELCARLOS1321@users.noreply.github.com>
2023-09-14 10:46:52 +0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -9,11 +9,19 @@ export default function Home() {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>[I AM A HONOR STUDENT FROM UNIVERSITY OF THE ASSUMPTION!]</p>
-        <p>My name is Miguel Francis M. Carlos from San Simon Pampanga 20yrs old. My hobbies are gaming,
-painting, programming, editing, making videos, etc.. My favorite music is a chill type of music like
-some chill rap like Mona Lisa. My Favorite Food is definitely any food that you can partner up with an
-egg.</p>
+        <p>10 Things That Require Zero Talent</p>
+        <ul>
+        <li>::marker "1. Being on Time"</li>
+        <li>::marker "2. Making An Effort"</li>
+        <li>::marker "3. Being High Energy"</li>
+        <li>::marker "4. Having A Positive Attitude"</li>
+        <li>::marker "5. Being Passionate"</li>
+        <li>::marker "6. Using Good Body Language"</li>
+        <li>::marker "7. Being Coachable"</li>
+        <li>::marker "8. Doing A Little Extra"</li>
+        <li>::marker "9. Being Prepared"</li>
+        <li>::marker "10. Having A Strong Work Ethic"</li>
+        </ul>
         <p>
           (This is a sample website - you’ll be building a site like this on{' '}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
