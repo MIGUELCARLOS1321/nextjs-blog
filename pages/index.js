5ae050d5c268f3fc4b224c7d4e231e7b999abba7
MIGUELCARLOS1321 <144299046+MIGUELCARLOS1321@users.noreply.github.com>
2024-02-15 11:20:43 +0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,12 @@
 import Head from 'next/head';
-import Layout, {siteTitle } from '../components/layout';
+import Layout, { No Talent Required } from '../components/layout';
 import utilStyles from '../styles/utils.module.css';
 
 export default function Home() {
   return (
     <Layout home>
       <Head>
-        <title>{siteTitle}</title>
+        <title>{No Talent Required}</title>
       </Head>
     <title>No Talent Required</title>
       <section className={utilStyles.headingMd}>
